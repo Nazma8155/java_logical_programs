3b92060280a80f8656a3cc46a26796b27cab66f7
Nazma8155 <sknazma8166@gmail.com>
2024-11-09 21:13:49 +0530

    Add files via upload

---
+++ srk mca A/day 1/Employe.java
@@ -0,0 +1,13 @@
+class Employe
+{
+    public static void main(String args[])
+    {
+    int empno=35;
+    String empname="jaswanth";
+    ;
+    float empsalary=45000;
+    System.out.println("the given employe no is:"+empno);
+    System.out.println("the given employe name is:"+empname);
+    System.out.println("the given employee salary is:"+empsalary);
+    }
+}
